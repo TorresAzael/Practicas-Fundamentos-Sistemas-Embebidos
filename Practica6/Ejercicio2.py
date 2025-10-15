7d988ad3d1ac97df4d1b680a58ef52a6e82223aa
TorresAzael <48043614+TorresAzael@users.noreply.github.com>
2025-10-15 16:58:04 -0600

    Add files via upload
    Versión Final - Ejercicios 1 y 2

--- Practica6/Ejercicio2.py
+++ Practica6/Ejercicio2.py
@@ -13,11 +13,12 @@ import time
 import matplotlib.pyplot as plt
 import csv
 from datetime import datetime
+import os
 # RP2040 I2C device address
 SLAVE_ADDR = 0x0A # I2C Address of RP2040
 
 # Name of the file in which the log is kept
-LOG_FILE = './temp.log'
+LOG_FILE = './temp.txt'
 
 # Initialize the I2C bus;
 # RPI version 1 requires smbus.SMBus(0)
@@ -47,10 +48,29 @@ def read_Date_Today():
 
 def log_temp_date(temperature, date):
 	try:
-		with open(LOG_FILE, "w") as fp:
+		with open(LOG_FILE, "a") as fp:
 			fp.write('{},{}:{}:{} \n'.format(temperature, date.hour, date.minute, date.second))
 	except:
 		return
+
+def graph_TempDate(Date):
+
+	x = []
+	y = []
+
+	with open('temp.txt','r') as fp:
+		plots = csv.reader(fp, delimiter=',')
+		for row in plots:
+			x.append(row[1])
+			y.append(float(row[0]))
+
+	plt.plot(x,y, label='Variación de la temperatura')
+	plt.xlabel('Tiempo - Fecha De Hoy:{Dia}/{Mes}/{Año}'
+			.format(Dia=Date.day,Mes=Date.month,Año=Date.year))
+	plt.ylabel('Temperatura')
+	plt.title('Gráfica Lectura de Temperatura con RP2040')
+	plt.legend()
+	plt.show()
 
 def main():
 	while True:
@@ -60,11 +80,13 @@ def main():
 
 			log_temp_date(cTemp, tDate)
 
-			print('Temperatura recibida:{varTemp},{varHrs}:{varMinu}:{varSeg}'
+			print('Temperatura recibida:{varTemp}°C,{varHrs}:{varMinu}:{varSeg}'
 					.format(varTemp=cTemp, varHrs=tDate.hour, varMinu=tDate.minute,varSeg=tDate.second))
 			time.sleep(1)
 
 		except KeyboardInterrupt:
+			graph_TempDate(tDate)
+			os.remove('temp.txt')
 			return
 
 if __name__ == '__main__':
